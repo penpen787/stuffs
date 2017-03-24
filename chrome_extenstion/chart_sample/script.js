b2c601c408debab74629be576b9ccd36af91c730
Kyunghun Jeon <penpen787@gmail.com>
2017-03-24 21:22:30 +0900

    chrome extenstion sample-6 코드정리

--- chrome_extenstion/chart_sample/script.js
+++ chrome_extenstion/chart_sample/script.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {   // Load the function after DOM ready.
 
-    // var link1=chrome.extension.getURL("s_icon.png");     //Get absolute path of the file residing your extension.
-    // var t1='<div id="pop"><a href=""><img id="mgt" src="'+link1+'"width="25" height="24"></a></div>'    //now set the src to absolute path.
-    // $("#eow-title").prepend(t1);     //Insert MailGet icon into top-right corner of Gmail home.
-
     var htmlElements ='<div id="chartOuterDiv"><canvas id="chartDiv" width="600" height="100"></canvas></div>'    //now set the src to absolute path.
     $("#watch-header").before(htmlElements);     //Insert MailGet icon into top-right corner of Gmail home.
 
