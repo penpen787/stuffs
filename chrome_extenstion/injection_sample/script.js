8709ecb574d2452bbf7d983ab5010de9c2bdaf5a
Kyunghun Jeon <penpen787@gmail.com>
2017-03-26 14:42:04 +0900

    chrome extenstion sample-6 코드정리-2

--- chrome_extenstion/injection_sample/script.js
+++ chrome_extenstion/injection_sample/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {   // Load the function after DOM ready.
-
     var link1=chrome.extension.getURL("s_icon.png");     //Get absolute path of the file residing your extension.
     var t1='<div id="pop"><a href=""><img id="mgt" src="'+link1+'"width="25" height="24"></a></div>'    //now set the src to absolute path.
     $("#eow-title").prepend(t1);     //Insert MailGet icon into top-right corner of Gmail home.
